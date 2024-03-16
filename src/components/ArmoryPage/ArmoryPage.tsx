139370d19894b4f59254f8fa80ed3de987a57abf
Ruslan Batsulia <ruslan.batsulia@gmail.com>
2024-03-16 17:18:39 +0200

    Add season page

--- src/components/ArmoryPage/ArmoryPage.tsx
+++ src/components/ArmoryPage/ArmoryPage.tsx
@@ -4,9 +4,9 @@ export default function ArmoryPage() {
   return(
     <div className="armory-page">
       <div className="container armory-page__container">
-        <p className="home-page__first-line">БЕЗ ЛІЦЕНЗІЇ</p>
-        <p className="home-page__second-line">і довідки від психіатра</p>
-        <p className="home-page__thrid-line">МОЖЕШ ВАЛИТИ ЗВІДСИ</p>
+        <p className="armory-page__first-line">БЕЗ ЛІЦЕНЗІЇ</p>
+        <p className="armory-page__second-line">і довідки від психіатра</p>
+        <p className="armory-page__thrid-line">МОЖЕШ ВАЛИТИ ЗВІДСИ</p>
       </div>
     </div>
   )
---
+++ src/components/SeasonPage/SeasonPage.tsx
@@ -0,0 +1,13 @@
+import "./SeasonPage.scss";
+
+export default function SeasonPage() {
+    return (
+        <div className="season-page">
+            <div className="container season-page__container">
+                <p className="season-page__first-line">ЛІТНІЙ СЕЗОН</p>
+                <p className="season-page__second-line">офіційно</p>
+                <p className="season-page__thrid-line">ВІДКРИТО</p>
+            </div>
+        </div>
+    );
+}
